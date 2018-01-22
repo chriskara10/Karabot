ad90cf252d6b6f11e41666e66dcdfeeeefc5580d
Kepler <35268152+chriskara10@users.noreply.github.com>
2018-01-22 13:54:53 +0100

    !new
    adding !new

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on('ready', async () => {
   console.log('Bot is ready! ${bot.user.username}');
-  
+
   try {
 	  let link = await bot.generateInvite(["ADMINISTRATOR"]);
 	  console.log(link);
@@ -29,13 +29,13 @@ bot.on('guildMemberAdd', member => {
 bot.on("message", async message => {
 	if(message.author.bot) return;
 	if(message.channel.type === "dm") return;
-	
+
 	let messageArray = message.content.split(" ");
 	let command = messageArray[0];
 	let args = messageArray.slice(1);
-	
+
 	if(!command.startsWith(botSettings.prefix))return;
-	
+
 	if(command == '!user') {
 		let embed = new Discord.RichEmbed()
 		.setAuthor(message.author.username)
@@ -44,10 +44,22 @@ bot.on("message", async message => {
 		.addField('Full Username', `${message.author.username}#${message.author.discriminator}`)
 		.addField("ID", message.author.id)
 		.addField("Created At", message.author.createdAt)
-		
+
 		message.channel.sendEmbed(embed);
 	}
-	
+
+// new command testing
+
+  if(command == '!new') {
+    let embed = new Discord.RichEmbed()
+    .setColor("#3259B6")
+    .addField('Full Username', `${message.author.username}#${message.author.discriminator}`)
+    .addField("Member Count", message.guild.memberCount)
+
+    message.channel.sendEmbed(embed);
+  }
+
+
 });
 
 bot.login(botSettings.token);
